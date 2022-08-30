295d695a8f760faf9f06fc4957167d089b6656dd
omniah7 <om267165@gmail.com>
2022-08-31 00:28:29 +0200

    .format

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -6,4 +6,4 @@ def uppercase(str):
         if (ord(c) >= ord('a')) and (ord(c) <= ord('z')):
             tmp[i] = chr(ord(c) - ord('a') + ord('A'))
         i += 1
-    print(f"{''.join(tmp)}")
+    print("{}".format(''.join(tmp)))
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number < 0: number *= -1
+    if number < 0:
+        number *= -1
+
     last = number % 10
-    print(last, end='')
+    print('{}'.format(last), end='')
     return (last)
