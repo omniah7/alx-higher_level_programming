ea2eb5be9f2fdbba111e9ea7ad67dac8ebfd3116
om267165@gmail.com <om267165@gmail.com>
2022-08-31 00:33:31 +0200

    .format

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for j in range(i+1, 10):
-        print('{}'.format{i}, end='')
-        print('{}'.format{j}, end=', ' if (i != 8) else '\n')
+        print('{}'.format(i), end='')
+        print('{}'.format(j), end=', ' if (i != 8) else '\n')
