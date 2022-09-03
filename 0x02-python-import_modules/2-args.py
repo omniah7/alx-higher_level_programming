a4264fd2eea41a7343689e571ce1bad0da39cd07
om267165@gmail.com <om267165@gmail.com>
2022-09-03 16:17:04 +0200

    added missing :

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,11 @@ if __name__ == '__main__':
     from sys import argv
 
     argc = len(argv)
-    if (argc == 1)
+    if (argc == 1):
         print(f"0 arguments.")
-    elif (argc == 2)
+    elif (argc == 2):
         print(f"1 argument:")
-    else
+    else:
         print(f"{argc - 1} arguments:")
 
     for i in range(1, argc):
