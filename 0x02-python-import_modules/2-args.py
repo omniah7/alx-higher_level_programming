fb5da20afa525a9bb6128d39c2d483deff7a3664
om267165@gmail.com <om267165@gmail.com>
2022-09-03 16:06:59 +0200

    for correct output

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,12 @@ if __name__ == '__main__':
     from sys import argv
 
     argc = len(argv)
-    print(f"{argc - 1} arguments", end='.\n' if argc == 1 else ':\n')
+    if (argc == 1)
+        print(f"0 arguments.")
+    elif (argc == 2)
+        print(f"1 argument:")
+    else
+        print(f"{argc - 1} arguments:")
 
     for i in range(1, argc):
         print(f"{i}: {argv[i]}")
