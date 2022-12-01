4cd6fde10ad3c048783b0980cc484ea766db2c04
omniah7 <om267165@gmail.com>
2022-12-01 20:44:52 +0200

    trying another code

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
+
+if __name__ == "__main__":
+    """Handle basic arithmetic operations."""
     from calculator_1 import add, sub, mul, div
-    from sys import argv
+    import sys
 
-    argc = len(argv)
-    if argc != 4:
+    if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    opr = argv[2]
-    if opr not in "+-*/":
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-    a, b = int(argv[1]), int(argv[3])  # cast a, b into integers
-    ops = {
-        '+': add,
-        '-': sub,
-        '*': mul,
-        '/': div,
-        }
+        sys.exit(1)
 
-    print("{:d} {:s} {:d} = {:d}".format(a, opr, b, ops[opr](a, b)))
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
