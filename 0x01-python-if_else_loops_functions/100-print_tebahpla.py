31039d0b9f3bdba5e7a6d16fa0f660773d6883a5
omniah7 <om267165@gmail.com>
2022-12-01 03:05:17 +0200

    another solution

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 for i, j in zip(range(122, 96, -2), range(121, 96, -2)):
-    print(f"{chr(i).lower()}{chr(j).upper()}", end='')
+    print("{}{}".format(chr(i).lower(), chr(j).upper()), end='')
