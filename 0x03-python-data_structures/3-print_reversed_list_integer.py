f491b8876e86d9f2bf2a6941e9c2687f426b0e81
omniah7 <om267165@gmail.com>
2022-12-03 13:03:45 +0200

    add :d and other fixes to all

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    #last = len(my_list) - 1
     for i in reversed(range(0, len(my_list))):
-        print ("{}".format(my_list[i]))
+        print("{:d}".format(my_list[i]))
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,5 +6,5 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         last = len(row) - 1  # last element in row
         for i in range(0, last):
-            print("{}".format(row[i]), end=' ')
-        print("{}".format(row[last]))  # print last element and '\n'
+            print("{:d}".format(row[i]), end=' ')
+        print("{:d}".format(row[last]))  # print last element and '\n'
