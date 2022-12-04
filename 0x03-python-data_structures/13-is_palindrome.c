af5d0f7828a6590f794ac206ba01f07682302fd7
omniah7 <om267165@gmail.com>
2022-12-04 19:28:49 +0200

    Linked list palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - frees a listint_t list
+ * @head: pointer to pointer of first node of listint_t list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *current = (*head);
+	int *list;
+	int i;
+
+	if (current == NULL || current->next == NULL)
+	{
+		return (1);
+	}
+
+	for (i = 0; current; i++, current = current->next)
+	{
+		if (i == 0) /* first iteration */
+		{
+			list = malloc(sizeof(int));
+			if (list == NULL)
+			{
+				return (-1);
+			}
+			list[0] = current->n;
+			continue;
+		}
+		list = realloc(list, (i + 1) * sizeof(int));
+		list[i] = current->n;
+	}
+
+	for (int j = 0; j < i / 2; j++)
+	{
+		if (list[j] != list[i - j - 1])
+		{
+			free(list);
+			return (0);
+		}
+	}
+	return (1);
+}
---
+++ 0x03-python-data_structures/13-main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * main - check the code for
+ *
+ * Return: Always 0.
+ */
+int main(void)
+	{
+	listint_t *head;
+	int arr1[] = {1, 17, 972, 50, 98, 98, 50, 972, 17, 1};
+	int arr2[] = {3};
+
+	head = NULL;
+	for (int i = 0; i < 10; i++)
+	{
+		add_nodeint_end(&head, arr1[i]);
+	}
+	print_listint(head);
+
+	if (is_palindrome(&head) == 1)
+		printf("Linked list is a palindrome\n");
+	else
+		printf("Linked list is not a palindrome\n");
+
+	free_listint(head);
+
+	return (0);
+}
