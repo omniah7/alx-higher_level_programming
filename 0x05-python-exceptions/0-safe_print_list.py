c3f3c06d23b4948fefaca7b70fa88b46d5d23c66
omniah7 <om267165@gmail.com>
2022-12-19 17:31:58 +0200

    tasks 0-4

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        n = 0
+        for e in range(x):
+            print(my_list[e], end="")
+            n += 1
+        print()
+        return n
+    except IndexError:
+        print()
+        return n
+        
