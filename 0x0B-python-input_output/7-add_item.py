bd0bfaaaf263d33274eea50f1eac43ee99ffc19d
omniah7 <om267165@gmail.com>
2023-01-10 18:41:59 +0200

    fix 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-
+"""adds all arguments to a Python list,
+and then save them to a file
+"""
 import sys
 save = __import__('5-save_to_json_file').save_to_json_file
 load = __import__('6-load_from_json_file').load_from_json_file
 
 
-"""adds all arguments to a Python list,
-and then save them to a file
-"""
 
-filename = "add_item.json"
-newlist = sys.argv[1:]
-try:
-    oldata = list(load(filename))
-except FileNotFoundError:
-    f = open(filename, 'x', encoding="UTF8")
-    f.close()
-    oldata = []
-oldata += newlist
-save(oldata, filename)
+if __name__ == '__main__':
+    filename = "add_item.json"
+    newlist = sys.argv[1:]
+    try:
+        oldata = list(load(filename))
+    except FileNotFoundError:
+        f = open(filename, 'x', encoding="UTF8")
+        f.close()
+        oldata = []
+    oldata += newlist
+    save(oldata, filename)
