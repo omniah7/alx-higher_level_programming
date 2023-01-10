00b7c7667c3692a38acf0450bb226f89e8cc482e
omniah7 <om267165@gmail.com>
2023-01-10 18:49:59 +0200

    pycodestyle 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,8 +6,6 @@ import sys
 save = __import__('5-save_to_json_file').save_to_json_file
 load = __import__('6-load_from_json_file').load_from_json_file
 
-
-
 if __name__ == '__main__':
     filename = "add_item.json"
     newlist = sys.argv[1:]
