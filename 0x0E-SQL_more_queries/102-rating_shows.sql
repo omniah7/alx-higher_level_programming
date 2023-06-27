4f4d07d3b7a747cb9fbe5fcf5ba191822caaa5f5
omniah7 <om267165@gmail.com>
2023-06-28 00:45:35 +0300

    some fixes

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,5 @@
 -- lists all shows from hbtn_0d_tvshows_rate by their rating.
-SELECT tv_shows.title, SUM(r.rate)
+SELECT tv_shows.title, SUM(r.rate) 'rating'
     FROM tv_shows INNER JOIN tv_show_ratings r
     ON tv_shows.id = r.show_id
     GROUP BY tv_shows.title
--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,5 +1,5 @@
 -- lists all genres in the database hbtn_0d_tvshows_rate by their rating.
-SELECT tv_genres.name, SUM(r.rate)
+SELECT tv_genres.name, SUM(r.rate) 'rating'
     FROM tv_genres INNER JOIN tv_show_genres sh ON tv_genres.id = sh.genre_id
     INNER JOIN tv_shows ON tv_shows.id = sh.show_id
     INNER JOIN tv_show_ratings r ON r.show_id = sh.show_id
