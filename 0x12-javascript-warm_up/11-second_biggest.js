fec29d60d6e19a190d8dadd531ed76a6aa2ab7dd
omniah7 <om267165@gmail.com>
2023-07-01 21:30:11 +0300

    11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-const argv =new Int16Array(process.argv.slice(2));
+const argv = new Int16Array(process.argv.slice(2));
 const args = argv.length;
 if (args <= 1) {
   console.log(0);
 } else {
-  const ls = argv.sort();
-  console.log(ls[args - 2]);
+  console.log(argv.sort()[args - 2]);
 }
