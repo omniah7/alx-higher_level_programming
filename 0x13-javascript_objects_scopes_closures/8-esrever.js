848cfc7dd7d5267ef228786c3a307557945e1605
omniah7 <om267165@gmail.com>
2023-08-16 05:07:25 +0300

    deleted the if condition from 8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  if (list === undefined || list.length === 0) {
-    return;
-  }
   const rev = [];
   for (let i = list.length - 1; i >= 0; i--) {
     rev.push(list[i]);
